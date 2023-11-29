1719940d35f0deb1226e4a9735480c9418beffe0
hiimminjeong <jmjeong38@gmail.com>
2023-11-29 15:53:22 +0900

    map 오류 수정

--- app/src/main/java/com/example/mobilesoftware/MapFragment.kt
+++ app/src/main/java/com/example/mobilesoftware/MapFragment.kt
@@ -1,18 +1,46 @@
 package com.example.mobilesoftware
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-class MapFragment : Fragment() {
+    private lateinit var mMap: GoogleMap
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_map, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return view
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val seoul = LatLng(37.556, 126.97)
+
+        val markerOptions = MarkerOptions()
+        markerOptions.position(seoul)
+        markerOptions.title("서울")
+        markerOptions.snippet("한국 수도")
+
+        mMap.addMarker(markerOptions)
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul, 10f))
+    }
 }
