8e36bdbc17fd2454b66d8a6c7fe169b0f657202d
soo5o <jungsk20@gmail.com>
2023-12-13 21:49:25 +0900

    HomeActivity 최종

--- app/src/main/java/com/example/mobilesoftware/HomeActivity.kt
+++ app/src/main/java/com/example/mobilesoftware/HomeActivity.kt
@@ -3,26 +3,64 @@ package com.example.mobilesoftware
 import RunFragment
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.mobilesoftware.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class HomeActivity : AppCompatActivity() {
     lateinit var bottomNav : BottomNavigationView
     lateinit var toggle: ActionBarDrawerToggle
+    val userId = MyApplication.auth.currentUser?.uid
     override fun onCreate(savedInstanceState: Bundle?) {
         val binding = ActivityHomeBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         loadFragment(RunFragment())
+        val updateNh = {
+            val user = Firebase.auth.currentUser
+            val imgRef = MyApplication.storage.reference.child("images/${userId}.jpg")
+            imgRef.downloadUrl.addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    Glide.with(this)
+                        .load(task.result)
+                        .into(binding.mainDrawerView.getHeaderView(0).findViewById(R.id.profileImg) as ImageView)
+                }
+            } .addOnFailureListener{
+                Log.d("runTo", "imgref file save error", it)
+            }
+            MyApplication.db.collection("users")
+                .document("${user?.uid}")
+                .get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val nickname = document.data?.get("nickname").toString()
+                        val headerView = binding.mainDrawerView.getHeaderView(0)
+                        val userName = headerView.findViewById(R.id.nav_nickname) as TextView
+                        userName.text = nickname
+                    } else {
+                        Log.d("runTo", "No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d("runTo", "get failed with ", exception)
+                }
+        }
+        updateNh()
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
         bottomNav.setSelectedItemId(R.id.run);
         bottomNav.setOnItemSelectedListener {
+            updateNh()
             when (it.itemId) {
                 R.id.map -> {
                     loadFragment(MapFragment())
@@ -62,9 +100,11 @@ class HomeActivity : AppCompatActivity() {
             when (menuItem.itemId) {
                 R.id.edit_profile -> {
                     loadFragment(EditProfileFragment())
+                    updateNh()
                 }
                 R.id.setting -> {
                     loadFragment(SettingFragment())
+                    updateNh()
                 }
                 R.id.logout -> {
                     MyApplication.auth.signOut()
